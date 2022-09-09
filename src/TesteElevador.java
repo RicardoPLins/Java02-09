f3316153b7a44eb963aea070191e0511f59e856b
RicardoPLins <79940823+RicardoPLins@users.noreply.github.com>
2022-09-09 16:20:53 -0300

    Update TesteElevador.java

--- src/TesteElevador.java
+++ src/TesteElevador.java
@@ -1,20 +1,20 @@
-
 public class TesteElevador {
 
 	public static void main(String[] args) {
-		Elevador e1 = new Elevador(10);
-		e1.irPara( 5);
-		System.out.println(e1.Andar_atual);
-		System.out.println(e1.quant_Andares);
-		e1.irPara(3);
-		System.out.println(e1.Andar_atual);
-		
-		Elevador elevador = new Elevador (10);
-		System.out.println(elevador.Andar_atual);
+		Elevador elevador = new Elevador (20);
+		System.out.println(elevador.getAndar_atual());
 		int[] andares;
+		andares = elevador.irPara(22);
+		for (int i=0; i<andares.length; i++)
+		System.out.println("=>" + andares[i]);
+		System.out.println(elevador.getAndar_atual());
+		andares = elevador.irPara(18);
+		for (int i=0; i<andares.length; i++)
+			System.out.println("=>" + andares[i]);
+		System.out.println(elevador.getAndar_atual());
 		andares = elevador.irPara(5);
-		for (int i=0; i<andares.length; i++) {
-			System.out.println("=>"+ andares[i]);}
-		
+		for (int i=0; i<andares.length; i++)
+			System.out.println("=>" + andares[i]);
+		System.out.println(elevador.getAndar_atual());
 	}
 }
